c8f5b1564727447ff40d04d40528528cdd1e9a90
David Moreno <davidmm@everymundo.com>
2021-06-04 12:44:54 -0400

    create range rule form and view

--- src/components/form/RangeRuleForm.jsx
+++ src/components/form/RangeRuleForm.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { usePassengers } from '../../context/PassengerContext';
+import PaxSelector from '../controls/PaxSelector';
+import FormControls from '../controls/FormControls';
 
 const RangeRuleForm = ({
   id = '',
@@ -6,24 +9,28 @@ const RangeRuleForm = ({
   min = 0,
   max = 0,
   handleChange = () => {},
-  handleRemove= () => {}
+  handleRemove= () => {},
+  handleSave = ()=> {},
 }) => {
+  const {passengers = []} = usePassengers();
+  const handleSaveRule = (event) => {
+    event.preventDefault();
+    handleSave(id)
+  }
   return (
-    <div className="grid grid-cols-1 mt-5 mx-4">
-      <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Input 1</label>
+    <div className="grid grid-cols-1 mt-5 mx-4 w-3/4">
+      <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Passenger</label>
       <div className="flex items-center">
-        <input 
+        <PaxSelector
           id={id}
           name={'pax'}
-          className="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
-          type="text" placeholder="age1"
-          value={pax}
-          onChange={handleChange}
-          />
+          selectedValue={pax}
+          handleChange={handleChange}
+        />
         <input 
           id={id}
           name={'min'}
-          className="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
+          className=" flex w-full mr-1 lg:w-1/4 py-1 px-3 rounded-md border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
           type="number" placeholder="From"
           value={min}
           onChange={handleChange}
@@ -31,23 +38,14 @@ const RangeRuleForm = ({
         <input 
           id={id}
           name={'max'}
-          className="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
+          className=" flex w-full mr-1 lg:w-1/4 py-1 px-3 rounded-md border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
           type="number" placeholder="From"
           value={max}
           onChange={handleChange}
         />
-        <button
-            className="inline-block p-3 text-center text-white transition bg-red-500 rounded-full shadow ripple hover:shadow-lg hover:bg-red-600 focus:outline-none ml-4"
-            onClick={() => handleRemove(id)}
-          >
-          <svg className="w-5 h-5 text-white fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path
-              fillRule="evenodd"
-              d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </button>
+        <FormControls
+          handleSave={ () => handleSave(id)}
+        />
       </div>
     </div>
   );
--- src/components/views/RangeRuleView.jsx
+++ src/components/views/RangeRuleView.jsx
@@ -1,18 +1,31 @@
 import React from 'react';
+import ViewControls from '../controls/ViewControls';
 
 const RangeRuleView = ({
   id = '',
   pax = '',
   min = 0,
   max = 0,
+  handleEdit = () => {},
+  handleRemove = () => {},
 }) => {
   return (
-    <li id={id} className="flex justify-between text-sm">
-      <span>{min}</span>
-       <span>{'<='}</span>
-      <div className="span"> {pax}</div>
-      <span>{'<='}</span>
-      <span>{max}</span>
+    <li id={id} className="flex justify-between text-sm w-full lg:w-3/4">
+      <div className="flex w-full items-center text-lg font-medium">
+        <span>{min}</span>
+        <span>{'<='}</span>
+        <span > {pax}</span>
+        <span>{'<='}</span>
+        <span>{max}</span>
+
+      </div>
+      <div className="flex w-full">
+        <ViewControls
+          id={id}
+          handleEdit={handleEdit}
+          handleRemove={handleRemove}
+        />
+      </div>
     </li>
   );
 };
