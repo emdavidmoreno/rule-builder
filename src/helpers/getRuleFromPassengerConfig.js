74a7b3739db2473bcbf838f84bb30ec3b7f887f2
David Moreno <davidmm@everymundo.com>
2021-06-04 13:54:31 -0400

    refactoring funtion to render rules as string.

--- src/helpers/getRuleFromPassengerConfig.js
+++ src/helpers/getRuleFromPassengerConfig.js
@@ -1,11 +1,13 @@
+import { SUM_PAX_VS_PAX_RULE, PAX_VS_PAX_RULE, RANGE_RULE, SIMPLE_RULE, SUM_PAX_VS_NUMBER_RULE, SUM_PAX_VS_SUM_PAX_RULE, TOTAL_RULE } from "../constants";
+
 function totalRule (total = 0, passengers = []) {
   if(!passengers.length) return null;
-  const variables = passengers.map((p = {}) => `"${p.name || ''}"`).join(',');
+  const variables = passengers.map((pax = {}) => `{"var": "${pax || ''}"}`).join(',');
   
   return `{"<=":[{"+":[${variables}]}, ${total}]}`
 }
 
-export const rangeRule =  ({pax = '', min = 0, max = 0}) => {
+export const rangeRule = (pax = '', min = 0, max = 0) => {
   return `{"and": [{">=":[{"var": "${pax}"}, ${min}]}, {"<=":["var": "${pax}", ${max}]}]}`;
 }
 
@@ -18,4 +20,32 @@ export default function rulesSTR (total = 0, passengers = []) {
   ${passengers.map((p = {}) => rangeRule(p)).join(',\n')}
 ]}`
   )
+}
+
+export const convertRulesToString = (rules =[]) => {
+  const stringRules = rules.map(rule => {
+    switch (rule.type) {
+      case TOTAL_RULE:
+        return totalRule(rule.total, rule.paxs);
+      case RANGE_RULE:
+        return rangeRule(rule.pax, rule.min, rule.max);
+      case SIMPLE_RULE:
+        return 'simple rule string';
+      case PAX_VS_PAX_RULE:
+        return 'pax vs pax rule'
+      case SUM_PAX_VS_NUMBER_RULE:
+        return 'sum pax vs pax rule'
+      case SUM_PAX_VS_SUM_PAX_RULE:
+        return 'sum pax vs sum pax rule'
+      case SUM_PAX_VS_PAX_RULE:
+        return 'sum pax vs pax rule';
+      default:
+        return '';
+    }
+  })
+  .join(',\n');
+  return `{
+"and":[
+  ${stringRules}
+]}`
 }
