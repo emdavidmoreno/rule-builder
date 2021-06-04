6176723b0caac7ab60f3852586b24d56490b87bb
David Moreno <davidmm@everymundo.com>
2021-06-04 12:43:38 -0400

    create controls

---
+++ src/components/controls/PaxSelector.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { usePassengers } from '../../context/PassengerContext';
+
+const PaxSelector = ({
+  id = '',
+  name = '',
+  selectedValue = '',
+  handleChange = () => {},
+}) => {
+  const {passengers = []} = usePassengers();
+  return (
+    <select 
+      id={id}
+      name={name}
+      className=" flex w-full mr-1 lg:w-1/4 py-1 px-3 rounded-md border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
+      value={selectedValue}
+      onChange={handleChange}
+    >
+      <option value="" />
+      {passengers.map((p, i)=>(
+        <option key={i} value={p.value}>{p.label}</option>
+      ))}
+    </select>
+  );
+};
+
+export default PaxSelector;
