0635ec92a2c354a28ae84dc6fdec65d4759e5e98
David Moreno <davidmm@everymundo.com>
2021-06-04 12:46:14 -0400

    application constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,6 @@
+/**
+ * Rule constants
+ */
 export const RANGE_RULE = 'RANGE_RULE';
 export const SIMPLE_RULE = 'SIMPLE_RULE';
 export const TOTAL_RULE = 'TOTAL_RULE';
@@ -5,4 +8,17 @@ export const PAX_VS_PAX_RULE = 'PAX_VS_PAX_RULE';
 export const PAX_VS_NUMBER_RULE = 'PAX_VS_NUMBER_RULE';
 export const SUM_PAX_VS_NUMBER_RULE = 'SUM_PAX_VS_NUMBER_RULE';
 export const SUM_PAX_VS_SUM_PAX_RULE = 'SUM_PAX_VS_SUM_PAX_RULE';
+
+/**
+ * Operator constants
+ */
+export const GREATER_THAN = '>';
+export const GREATER_THAN_OR_EQUAL_TO = '>=';
+export const LESS_THAN = '<';
+export const LESS_THAN_OR_EQUAL_TO = '<=';
+export const DOUBLE_EQUAL_TO = '==';
+
+export const ADDITION = '+';
+export const EQUAL_TO = '=';
+
 
