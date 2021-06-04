0af612236808c5399de617f8dd77c97789066355
David Moreno <davidmm@everymundo.com>
2021-06-04 12:45:35 -0400

    create total passenger form

--- src/components/form/TotalPassengerForm.jsx
+++ src/components/form/TotalPassengerForm.jsx
@@ -3,15 +3,18 @@ import React from 'react';
 const TotalPassengerForm = ({value, handleChange}) => {
   
   return (
-    <div className="flex flex-col mx-auto w-full lg:w-1/2">
-      <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Total passengers</label>
-      <input 
-        name={'totalPassengers'}
-        className="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
-        type="number" placeholder="Total passengers" 
-        value={value}
-        onChange={handleChange}
-        />
+    <div className="flex flex-col pb-4 w-full border-b w-full mb-3">
+      <div className="flex flex-col w-1/3">
+        <label className="uppercase md:text-sm text-xs text-gray-500 text-light font-semibold">Total passengers</label>
+        <input 
+          name={'totalPassengers'}
+          className="py-2 px-3 rounded-lg border-2 border-purple-300 mt-1 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" 
+          type="number" placeholder="Total passengers" 
+          value={value}
+          onChange={handleChange}
+          />
+
+      </div>
     </div>
   );
 };
