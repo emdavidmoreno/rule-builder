ea965533ec71ea94c6e1a5708587a2bc44306983
David Moreno <davidmm@everymundo.com>
2021-06-05 15:59:25 -0400

    refactoring passenger context

--- src/context/PassengerContext.js
+++ src/context/PassengerContext.js
@@ -5,7 +5,7 @@ const PassengerContext = createContext();
 export const PassengerProvider = ({children}) => {
   const [totalPassengers, setTotalPassengers] = useState(0);
   const [passengers, setPassengers] = useState([{value: 'age1', label: 'Adult'}]);
-  const [rulesString, setRulesString] = useState(0);
+  const [rulesString, setRulesString] = useState(null);
 
   return <PassengerContext.Provider
   value={{
