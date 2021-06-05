8951280229bf01448e12b60296cea248ea4c38b0
David Moreno <davidmm@everymundo.com>
2021-06-05 15:56:59 -0400

    create number selector control

---
+++ src/components/controls/NumberSelector.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const NumberSelector = ({
+  id = '',
+  name = '',
+  value = 0,
+  handleChange = () => {},
+}) => {
+  return (
+  <input 
+    id={id}
+    name={name}
+    className="flex w-full mr-1 py-1 px-3 max-h-10 rounded-md border-2 border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600 focus:border-transparent" 
+    type="number" 
+    placeholder="Number"
+    value={value}
+    onChange={(e) => handleChange(id, name , Number(e.target.value))}
+  />
+  );
+};
+
+export default NumberSelector;
--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -1,4 +1,5 @@
-export {default as FormControls} from './FormControls';
-export {default as ViewControls} from './ViewControls';
-export {default as OperatorSelector} from './selectors/OperatorSelector';
-export {default as PaxSelector} from './selectors/PaxSelector'
+export { default as FormControls } from './FormControls';
+export { default as ViewControls } from './ViewControls';
+export { default as OperatorSelector } from './selectors/OperatorSelector';
+export { default as PaxSelector } from './selectors/PaxSelector';
+export { default as NumberSelector } from './NumberSelector';
