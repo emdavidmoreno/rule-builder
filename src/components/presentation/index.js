3fe7c52eae16395efa88bcb038e68b79039a9aac
David Moreno <davidmm@everymundo.com>
2021-09-16 15:47:18 -0400

    refactoring dropdown action button

--- src/components/presentation/index.js
+++ src/components/presentation/index.js
@@ -1,4 +1,4 @@
 export { default as Logo } from './Logo';
 export { default as RuleViewer } from './RuleViewer';
-export { default as DropdownActionButton } from './DropdownActionButtons';
+export { default as DropdownActionButtons } from './DropdownActionButtons';
 export { default as Sidebar } from './sidebar/SideBar';
